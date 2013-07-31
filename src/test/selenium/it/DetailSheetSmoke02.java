a4a9a0929e58c172c4892b04a4366e1edf7b90b3
toshiS <sebalyn@mac.com>
2013-07-31 16:47:12 -0700

    Added sleep command to wait for detail sheet to be removed before its nonexistence is checked

--- src/test/selenium/it/DetailSheetSmoke02.java
+++ src/test/selenium/it/DetailSheetSmoke02.java
@@ -3,13 +3,14 @@ package test.selenium.it;
 public class DetailSheetSmoke02 extends TestBase {
 	
 	//2-1
-	public void test001(){
+	public void test001() throws InterruptedException{
 		selenium.open("/TreeOfLife/index.html");
 		assertEquals("Tree of Life", selenium.getTitle());
 		selenium.click("xpath=(//p[@id='dogName' and text()='Ann'])");
 		assertTrue(selenium.isElementPresent("//div[@id='detailSheet']/img[@src='img/dogs/boxer1.jpg']"));
 		assertTrue(selenium.isTextPresent("Ann"));
 		selenium.click("//div[@id='detailSheet']/img[@src='img/icon_close.png']");
+		Thread.sleep(1000);
 		assertFalse(selenium.isElementPresent("id=detailSheet"));
 	}
 }
